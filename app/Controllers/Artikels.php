2212b16bd25bf28a4faddc0f0281070f4bb7d3cf
Pieter De Praetere <pieter.de.praetere@helptux.be>
2025-03-13 21:26:56 +0100

    begin met het formulier voor één artikel

--- app/Controllers/Artikels.php
+++ app/Controllers/Artikels.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Models\KadasterModel;
 use App\Models\ArtikelModel;
 use App\Models\EigenaarModel;
+use App\Models\PerceelModel;
 
 class Artikels extends BaseController
 {
@@ -29,16 +30,29 @@ class Artikels extends BaseController
     {
         helper('form');
 
-        $input_data = $this->request->getPost([
-            'number',
+        $input_data = $this->request->getJSON();
+        /*
+        'number',
             'written_income',
             'written_size',
             'written_income_built',
             'kadaster_id',
             'eigenaar_name',
             'eigenaar_city',
-            'eigenaar_occupation'
-        ]);
+            'eigenaar_occupation',
+            'percelen' => [
+            {
+            'section',
+            'number',
+            'usage',
+            'size',
+            'label',
+            'income_unbuilt',
+            'income_built'
+            }
+            ]
+        */
+
 
         if (! $this->validateData($input_data, [
             'number'   => 'required|max_length[255]|min_length[1]',
@@ -48,15 +62,31 @@ class Artikels extends BaseController
             'kadaster_id' => 'required|integer',
             'eigenaar_name' => 'required|max_length[512]',
             'eigenaar_occupation' => 'max_length[512]',
-            'eigenaar_city' => 'max_length[255]'
+            'eigenaar_city' => 'max_length[255]',
+            'percelen' => 'field_exists'
         ])) {
             return $this->new();
         }
 
         $input_data = $this->validator->getValidated();
 
+        /* Validate percelen */
+        if (! $this->validateData($input_data['percelen'], [
+            'number'   => 'required|max_length[255]|min_length[1]',
+            'section'   => 'required',
+            'usage'   => 'required',
+            'size'   => 'required|decimal',
+            'type' => 'required',
+            'income_unbuilt'   => 'required|decimal',
+            'income_built'   => 'required|decimal',
+        ])) {
+            return $this->new();
+        }
+        $percelen = $this->validator->getValidated();
+
         $eigenaarModel = model(EigenaarModel::class);
         $artikelModel = model(ArtikelModel::class);
+        $perceelModel = model(PerceelModel::class);
 
         /* Check if we already have this eigenaar */
         /* We check on the combo name, city, occupation and pick the first one, as we have to pick someone */
@@ -73,6 +103,17 @@ class Artikels extends BaseController
             $eigenaar_id = $eigenaarModel->getInsertID();
         }
 
+        /* Compute the income and max size */
+        $computed_size = 0;
+        $computed_income = 0;
+        $computed_income_built = 0;
+
+        foreach ($percelen as $perceel) {
+            $computed_size = $computed_size + $perceel['size'];
+            $computed_income = $computed_income + $perceel['income_unbuilt'];
+            $computed_income_built = $computed_income_built + $perceel['income_built'];
+        }
+
         $artikelModel->save([
             'number' => $input_data['number'],
             'written_income' => $input_data['written_income'],
@@ -80,9 +121,25 @@ class Artikels extends BaseController
             'written_size' => $input_data['written_size'],
             'kadaster_id' => $input_data['kadaster_id'],
             'eigenaar_id' => $eigenaar_id,
-            'computed_income' => 0,
-            'computed_size' => 0
+            'computed_income' => $computed_income,
+            'computed_size' => $computed_size,
+            'computed_income_built' => $computed_income_built
         ]);
+
+        $artikel_id = $artikelModel->getInsertID();
+
+        foreach ($percelen as $perceel) {
+            $perceelModel->save([
+                'number' => $perceel['number'],
+                'section' => $perceel['section'],
+                'usage' => $perceel['usage'],
+                'size' => $perceel['size'],
+                'type' => $perceel['type'],
+                'income_unbuilt' => $perceel['income_unbuilt'],
+                'income_built' => $perceel['income_built'],
+                'artikel_id' => $artikel_id
+            ]);
+        }
 
         $data = [
             'title' => 'Nieuw artikel toevoegen'
--- app/Views/artikel/create.php
+++ app/Views/artikel/create.php
@@ -2,8 +2,9 @@
 <?= session()->getFlashdata('error') ?>
 <?= validation_list_errors('error_list') ?>
 
-<form action="/artikelen" method="post">
+<form>
     <?= csrf_field() ?>
+    <input type="hidden" id="percelen_count" value="1" />
     <div class="mb-3">
         <label for="number" class="form-label">Nummer van het artikel</label>
         <input type="text" class="form-control" id="number" name="number" value="<?= set_value('number') ?>" />
@@ -42,5 +43,54 @@
         <label for="eigenaar_occupation" class="form-label">Beroep</label>
         <input type="text" name="eigenaar_occupation" id="eigenaar_occupation" class="form-control" value="" />
     </div>
-    <button type="submit" class="btn btn-primary">Toevoegen</button>
+    <div id="percelen">
+        <div class="row mb-3">
+            <div class="col">
+                <label for="section_1" class="form-label">Sectie</label>
+                <input type="text" class="form-control" id="section_1" name="section" />
+            </div>
+            <div class="col">
+                <label for="number_1" class="form-label">Nummer</label>
+                <input type="text" class="form-control" id="number_1" name="number" />
+            </div>
+            <div class="col">
+                <label for="usage_1" class="form-label">Grondgebruik</label>
+                <select class="form-control" id="usage_1" name="usage">
+                    <option value="bouwland">Bouwland</option>
+                    <option value="tuin">Tuin</option>
+                    <option value="boomgaard">Boomgaard</option>
+                    <option value="hooiland">Hooiland</option>
+                    <option value="weiland">Weiland</option>
+                    <option value="bos">Bos</option>
+                    <option value="huis">Huis</option>
+                </select>
+            </div>
+            <div class="col">
+                <label for="size_1" class="form-label">Totale oppervlakte</label>
+                <input type="text" class="form-control" id="size_1" name="size" />
+            </div>
+            <div class="col">
+                <label for="type_1" class="form-label">Klasse</label>
+                <select class="form-control" id="type_1" name="type">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>
+            </div>
+
+            <div class="col">
+                <label for="income_unbuilt_1" class="form-label">Belastbaar inkomen (onbebouwd) - in fr.</label>
+                <input type="text" class="form-control" id="income_unbuilt_1" name="income_unbuilt" />
+            </div>
+            <div class="col">
+                <label for="income_built_1" class="form-label">Belastbaar inkomen (bebouwd) - in fr.</label>
+                <input type="text" class="form-control" id="income_built_1" name="income_built" />
+            </div>
+            <div class="col">
+                <button type="button" id="perceel_1" class="btn btn-primary">Nieuw perceel</button>
+            </div>
+        </div>
+    </div>
+    <button type="button" id="submit" class="btn btn-primary">Toevoegen</button>
 </form>
